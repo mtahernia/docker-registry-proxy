1963d13b757abd3b031d9ca51884647895f5e89e
aanthonyrizzo <arizzo@coreweave.com>
2023-05-15 20:27:28 -0400

    fix(liveliness): Follow curl redirects

--- liveliness.sh
+++ liveliness.sh
@@ -7,7 +7,7 @@ read -r -a repos <<< "$LIVELINESS_REPOS"
 for i in "${repos[@]}"
 do
   echo "repo $i liveliness check..."
-  response=$(curl --head -w '%{http_code}' -o /dev/null -s -k -x http://127.0.0.1:3128 "$i")
+  response=$(curl --head -L -w '%{http_code}' -o /dev/null -s -k -x http://127.0.0.1:3128 "$i")
   if [ "$response" -ne 200 ]; then
     echo "failed curl for repo $i"
     exit 1
