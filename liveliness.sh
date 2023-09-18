2cb5391968f72d71958fdf94367151406fd19578
ChandonPierre <80500072+ChandonPierre@users.noreply.github.com>
2023-09-18 16:37:23 -0400

    feat(liveness): print return code to stderr (#22)

--- liveliness.sh
+++ liveliness.sh
@@ -9,7 +9,7 @@ do
   echo "repo $i liveliness check..."
   response=$(curl --head -L -w '%{http_code}' -o /dev/null -s -k -x http://127.0.0.1:3128 "$i")
   if [[ "$response" -lt "200" ]] || [[ "$response" -ge "400" ]]; then
-    echo "failed curl for repo $i"
+    echo "failed curl for repo $i with response $response" >&2
     exit 1
   fi
 done
