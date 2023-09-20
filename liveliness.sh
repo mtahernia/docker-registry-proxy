fe2c308dd2d570b465bf0c5ef34059542f64254e
ChandonPierre <80500072+ChandonPierre@users.noreply.github.com>
2023-09-20 11:42:01 -0400

    feat(liveness): set curl timeout (#23)
    helps to fit in liveness probe total timeout

--- liveliness.sh
+++ liveliness.sh
@@ -7,7 +7,7 @@ read -r -a repos <<< "$LIVELINESS_REPOS"
 for i in "${repos[@]}"
 do
   echo "repo $i liveliness check..."
-  response=$(curl --head -L -w '%{http_code}' -o /dev/null -s -k -x http://127.0.0.1:3128 "$i")
+  response=$(curl --head -L -w '%{http_code}' -o /dev/null -s -k -x http://127.0.0.1:3128 "$i" --max-timeout "${CURL_TIMEOUT:-2}")
   if [[ "$response" -lt "200" ]] || [[ "$response" -ge "400" ]]; then
     echo "failed curl for repo $i with response $response" >&2
     exit 1
